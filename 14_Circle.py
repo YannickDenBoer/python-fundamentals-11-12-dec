7ce62798c4066868ff0187c6415ed91485d9108e
Yannick den Boer <95358977+YannickDenBoer@users.noreply.github.com>
2024-12-11 13:00:19 +0100

    Update 14_Circle.py

--- 14_Circle.py
+++ 14_Circle.py
@@ -9,11 +9,12 @@ area = math.pi * r**2
 # Bereken de omtrek
 circumference = 2 * math.pi * r
 
-#print("The circle has a radius:", r)
-#print("The area of the circle is", area)
-#print("The circumference of the circle is:", circumference)
+print("The circle has a radius:", r)
+print("The area of the circle is", area)
+print("The circumference of the circle is:", circumference)
 
-print("radiusString:", type(radiusString))
-print("r:", type(r))
-print("area:", type(area))
-print("circumference:", type(circumference))
+# Extra checks voor de types
+# print("radiusString:", type(radiusString))
+# print("r:", type(r))
+# print("area:", type(area))
+# print("circumference:", type(circumference))
